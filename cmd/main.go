148eb0796cf9026e84d151973d0e7cb6c332dfff
Adrien Delorme <adrien.delorme@icloud.com>
2020-03-19 15:56:42 +0100

    fix print call

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	gme "github.com/azr/go-modules-example"
+	"github.com/azr/go-modules-example/subpkg"
 )
 
 func main() {
-	fmt.Println("root pkg version is %v", gme.Version)
+	fmt.Println("root pkg version is ", gme.Version)
+	fmt.Println("sub pkg version is ", subpkg.Version)
 }
